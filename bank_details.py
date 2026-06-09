a857c01a1fb88ab2797e8a44dce74d3fbf412967
Rakesh3412 <rocky.dev3412@gmail.com>
2026-06-09 18:41:23 +0530

    updated

--- bank_details.py
+++ bank_details.py
@@ -1,4 +1,8 @@
 li=[]
+ph_num=[]
+adh_no=[]
+pan_noo=[]
+acc_no=[]
 def Bank():
 	print("______________________Bank account creation ______________________\n")
 	n=int(input("How many acoounts to create "))
@@ -9,54 +13,80 @@ def Bank():
 		pan_no=input("Enter pan number :")
 		account_no=int(input("Enter account number :"))
 		balance=500
-		emp={"Bank_name":"SBI","Ifsc_code":"IFCA0214RB","phone_number":ph_no,"address":add,"adhar_no":adhar_no,"pan_no":pan_no,"account_no":account_no,"Balance":balance}
-		li.append(emp)
-		print("\n_______________________________Next person account details____________________________\n")
-	print("___________________________________Bank book___________________________________________")
-	print("_________________________________________________________________________________________________________________________________________________")
-	print("Bank_name" "\t" "Ifsc_code" "\t" "Phone_number" "\t\t" "Address" "\t\t" "Adhar_no" "\t\t" "pan_no" "\t\t" "Account_no" "\t\t" "Balance" )
-	print("_________________________________________________________________________________________________________________________________________________")
-	for i in li:
-		print(i["Bank_name"],"\t\t",i["Ifsc_code"],"\t\t",i["phone_number"],"\t\t",i["address"],"\t",i["adhar_no"],"\t",i["pan_no"],"\t\t",i["account_no"],"\t\t", i["Balance"] )
+		if ph_no in ph_num:
+			print("number is already is existed ")
+		elif adhar_no in adh_no:
+			print("adhar no is existed ")
+		elif account_no in acc_no:
+			print("account number is existed ")
+		elif pan_no in pan_noo:
+			print("pan number is existed ")
+		else:
+			emp={"Bank_name":"SBI","Ifsc_code":"IFCA0214RB","phone_number":ph_no,"address":add,"adhar_no":adhar_no,"pan_no":pan_no,"account_no":account_no,"Balance":balance}
+			li.append(emp)
+			for j in li:
+				ph_num.append(j["phone_number"])
+				adh_no.append(j["adhar_no"])
+				pan_noo.append(j["pan_no"])
+				acc_no.append(j["account_no"])
+			print("___________________________________Bank book___________________________________________")
+			print("_________________________________________________________________________________________________________________________________________________")
+			print(f"{'BANK NAME':<10} | {'IFSC CODE':<15} | {'PHONE NUMBER':<12} | {'ADDRESS':<15} | {'ADHAR NO':<12} | {'PAN NO':<12} | {'ACCOUNT NO':<10} | {'BALANCE':<10}")
+			print("_________________________________________________________________________________________________________________________________________________")
+			for i in li:
+				print(f"{i['Bank_name']:<10} | {i['Ifsc_code']:<15} | {i['phone_number']:<12} | {i['address']:<15} | {i['adhar_no']:<12} | {i['pan_no']:<12} | {i['account_no']:<10} | {i['Balance']:<10}" )
 def deposit():
 	print("_________________________Deposit_____________________________\n")
-	acc=int(input("Select account "))
+	acc=int(input("Select account number: "))
+	if acc in acc_no:
+		print("valid account number ")
+	else:
+		print("Invalid account number ")
+
 	for i in li:
 		if acc == i["account_no"]  :
-			amt=int(input("Enter amount to deposit "))
-			i["Balance"]+=amt
-		else:
-			print("Account number invalid ")
+			amt=float(input("Enter amount to deposit must be positive "))
+			if amt>0:
+				i["Balance"]+=amt
+			else:
+				print("amt cannot be negative ")
 def withdraw():
 	print("_________________________Withdraw_____________________________\n")
 	acc=int(input("Select account number "))
+	if acc in acc_no:
+		print("valid account number ")
+	else:
+		print("Invalid account number ")
 	for i in li:
 		if acc == i["account_no"]:
-			with_draw=int(input("Enter amount to withdraw: "))
-			if with_draw<=i["Balance"]:
-				print("Transaction successfull ")
-				i["Balance"]-=with_draw
-				print("current balance ")
-				print(i["Balance"])
+			with_draw=float(input("Enter amount to withdraw must be positive : "))
+			if with_draw>0:
+				if with_draw<=i["Balance"]:
+					print("Transaction successfull ")
+					i["Balance"]-=with_draw
+					print("current balance ")
+					print(i["Balance"])
+				else:
+					print("Transaction faield ")
 			else:
-				print("Transaction faild ")
-		else:
-			print("Account number invalid ")
-
+				print("amount cannot be negative ")
 def check():
 	print("________________________________Balance check______________________\n")
-	acc=int(input("Select account number to check balance "))
+	acc=int(input("Select account number to check balance :"))
+	if acc in acc_no:
+		print("valid account number ")
+	else:
+		print("Invalid account number ")
 	for i in li:
 		if acc == i["account_no"]:
-			print(i["Balance"])
-	
+			print(i["Balance"])	
 print("______________________Menu____________________________________________\n")
-print("Select options 1.account_creation 2.exit \n")
+print("\nSelect options 1.account_creation 2.exit \n")
 sel=int(input("Select which operation to form "))
 if sel==1:
 	Bank()  
 	while True:
-		print("Select options  1.deposit 2.withdraw 3.balance_check 4.exit \n")
+		print("\nSelect options  1.deposit 2.withdraw 3.balance_check 4.exit \n")
 		inp=int(input("Select which operation to form "))
 		if inp==1:
 			deposit()
@@ -67,8 +97,13 @@ if sel==1:
 		elif inp==4:
 			print("Thank you visit again ")
 			break
+		else:
+			print("Invalid")
+			break
 elif sel==2:
 	print("Thank you vist again")
+else:
+	print("Invalid choice ")
 	
 
 
